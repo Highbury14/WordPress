92e58f6f795a1b27fa4ee6dcecf4fbf87ea41521
George.J <george.j@reflectionsinfos.com>
2021-04-19 03:48:18 +0530

    Theme-functions added.

--- wp-content/themes/tamkeen-theme/functions.php
+++ wp-content/themes/tamkeen-theme/functions.php
@@ -1,1 +1,17 @@
 <?php
+/**
+ * Functions and definitions
+ *
+ * @link https://reflectionsglobal.com
+ *
+ * @package WordPress
+ * @subpackage Tamkeen_Theme
+ */
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'tamkeen-theme-child-style', get_stylesheet_uri(),
+        array( 'twenty-twenty-one-style' ), 
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+    );
+}
